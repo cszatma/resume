8e38aac0b31d282ba19904c735b9424961ec9cb0
Christopher Szatmary <cs@christopherszatmary.com>
2020-01-12 23:49:03 -0500

    Publish ffd82fd9e03df94116521442f154653c0b965dda - 01-12-2020

---
+++ precache-manifest.752fc93369cd4e87095ad31da4225f88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c70d892ffbf37a60e4d154be7be430",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8388b702fe545e7de8eb",
+    "url": "/resume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ba73eaa880de48e90d00",
+    "url": "/resume/static/js/2.b99caa03.chunk.js"
+  },
+  {
+    "revision": "2fd894778850ff392c3045eb31eb86a2",
+    "url": "/resume/static/js/2.b99caa03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8388b702fe545e7de8eb",
+    "url": "/resume/static/js/main.c200ff5d.chunk.js"
+  },
+  {
+    "revision": "f01df9a6d656a3686d5c",
+    "url": "/resume/static/js/runtime-main.966029af.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/resume/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/resume/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
--- src/types/react-app-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare module '*.ttf' {
-  const src: string;
-  export default src;
-}
